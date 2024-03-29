910f1635a530fe74b5db9d2df8de1ce46521ec0e
telectr <alexcidon@icloud.com>
2024-03-29 12:22:30 -0400

    Remove unused function

--- util.ts
+++ util.ts
@@ -1,6 +1,5 @@
 import { exists } from "https://deno.land/std@0.219.1/fs/exists.ts";
 import { dirname } from "https://deno.land/std@0.219.1/path/dirname.ts";
-import { getVersionManifest } from "./api.ts";
 
 export const writeOnLine = (s: string) => {
   Deno.stdout.write(new TextEncoder().encode("\x1b[1K\r" + s));
@@ -22,13 +21,4 @@ export async function saveFile(data: string, path: string) {
     await Deno.mkdir(dir, { recursive: true });
   }
   await Deno.writeFile(path, new TextEncoder().encode(data));
-}
-
-export async function filterVersions(filter: string) {
-  if (!["release", "snapshot"].includes(filter)) {
-    throw TypeError(`${filter} is not a version type`);
-  }
-  return (await getVersionManifest()).versions
-    .filter((element) => element.type == filter)
-    .map((element) => element.id);
 }
