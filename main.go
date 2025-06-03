02dbbac42d269ea295aa3805dfcf84a11d36d0eb
telecter <alexcidon9@gmail.com>
2025-06-02 21:51:04 -0400

    First release! v1.0.0

--- main.go
+++ main.go
@@ -9,13 +9,25 @@ import (
 	"github.com/telecter/cmd-launcher/pkg/auth"
 )
 
+const VERSION = "1.0.0"
+const LICENSE = "Licensed MIT"
+const COPYRIGHT = "Copyright (c) 2024-2025 telecter"
+
+type version struct{}
+
+func (version) Run(ctx *kong.Context) error {
+	fmt.Printf("cmd-launcher %s\n%s\n%s\n", VERSION, COPYRIGHT, LICENSE)
+	return nil
+}
+
 type CLI struct {
-	Start  cmd.Start  `cmd:"" help:"Start the specified instance"`
-	Auth   cmd.Auth   `cmd:"" help:"Manage account authentication"`
-	Create cmd.Create `cmd:"" help:"Create a new Minecraft instance"`
-	Search cmd.Search `cmd:"" help:"Search versions and instances"`
-	Delete cmd.Delete `cmd:"" help:"Delete a Minecraft instance"`
-	Dir    string     `name:"dir" help:"Root directory to use for launcher" type:"path" placeholder:"PATH"`
+	Start   cmd.Start  `cmd:"" help:"Start the specified instance"`
+	Auth    cmd.Auth   `cmd:"" help:"Manage account authentication"`
+	Create  cmd.Create `cmd:"" help:"Create a new Minecraft instance"`
+	Search  cmd.Search `cmd:"" help:"Search versions and instances"`
+	Delete  cmd.Delete `cmd:"" help:"Delete a Minecraft instance"`
+	Version version    `cmd:"" help:"Display launcher version and about"`
+	Dir     string     `name:"dir" help:"Root directory to use for launcher" type:"path" placeholder:"PATH"`
 }
 
 func (c *CLI) AfterApply() error {
